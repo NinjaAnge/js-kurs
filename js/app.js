c2a92f0bfb23befe4561933331ad6020913d12a3
NinjaAnge <aleskj91@live.no>
2018-03-21 14:34:58 +0100

    hh

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 var heading = document.querySelector('h1');
-heading.textContent = "header lolz"
+var customHeader ="something";
+heading.textContent = customHeader;
+
+var number1 = 2.5;
+var number2 = 3;
+var texts ="fancy";
+var myList = ["one", "two", "three"]
+var myObject ={};
+
+
+function multyplyByTwo (n1, n2){
+   var val= n2+ n1
+return val * 2;
+}
+
+function addText(){
+ var elr = document.getElementById("textChange");
+
+ if (el.innerHTML === "rofl"){
+     el.innerHTML = null ;
+ } else {
+     el.innerHTML = "rofl"
+ }
+
+}
